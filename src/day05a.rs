6617000bc4d184675c6ca31842932cf6dfe340b1
zlRivo <tetjicraft@gmail.com>
2022-12-12 14:40:11 +0100

    Pseudo code

--- src/day05a.rs
+++ src/day05a.rs
@@ -2,4 +2,32 @@ use aoc2022::read_file;
 
 pub fn main() {
     let file = read_file!("./inputs/test_input05.txt");
+
+    /* ---------
+    PARSING
+    --------- */
+
+    // Find a way to loop until the row are the numbers
+
+    // Find the index of each number in the row
+
+    // Create n (biggest number) stacks (vectors) to store each letter
+
+    // For each number, get all the letters before current row with the same index in the line
+
+    // Skip until line is blank
+
+    /* ---------
+    PROCESSING
+    --------- */
+
+    // Split each line whitespace
+
+    // Get match 2 (amount), get match 4 (from) get match 6 (to)
+
+    // N (amount) times in , pop column from and push into column to
+
+    // Pop one letter from each stack and add to a string
+
+    // Return the string
 }
